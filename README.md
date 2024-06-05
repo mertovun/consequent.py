940600fc189963a3354ecd45d01f3e2cb44d0363
Mert Övün <mrtovun@gmail.com>
2024-06-05 14:28:18 +0300

    README

---
+++ README.md
@@ -0,0 +1,3 @@
+# consequent.py
+
+An audio effects script that fits sine wave oscillators to the spectrogram of an input WAV file and reconstructs it into glitched, warped, twisted, modulated, and manipulated exploits.
--- config.py
+++ config.py
@@ -13,15 +13,15 @@ class PeakSelection(Enum):
     LOWEST = 'lowest'
     RANDOM = 'random'
 
-SERIES = True
+SERIES = False
 
 SINE_FIT_CONFIGS = [
         {
         'N_FFT': 2048,
         'HOP_LENGTH': 4096,
-        'NUM_PEAKS': 4,
+        'NUM_PEAKS': 2,
         'PEAK_SELECTION_STRATEGY': PeakSelection.HIGHEST,
-        'N_PEAKS': 4,
+        'N_PEAKS': 2,
         'APPLY_HANNING': True,
         'HANNING_DEGREE': 0.6,
         'INCLUDE_PHASE': True,
@@ -39,9 +39,29 @@ SINE_FIT_CONFIGS = [
     {
         'N_FFT': 2048,
         'HOP_LENGTH': 4096,
-        'NUM_PEAKS': 16,
+        'NUM_PEAKS': 4,
         'PEAK_SELECTION_STRATEGY': PeakSelection.HIGHEST,
-        'N_PEAKS': 16,
+        'N_PEAKS': 4,
+        'APPLY_HANNING': True,
+        'HANNING_DEGREE': 0.6,
+        'INCLUDE_PHASE': True,
+        'PHASE_DEGREE': 0.5,
+        'APPLY_SCALE': False,
+        'BASE_NOTE': 'E',
+        'SCALING_COEFFICIENTS': [.99, .0, .6, .7, .0, .5, .1, .8, .6, .0, .4, .0],
+        'APPLY_GLIDING': False,
+        'GLIDE_THRESHOLD_MAX': 12.0,
+        'GLIDE_THRESHOLD_MIN': 5.0,
+        'CUTOFF_FREQUENCY': 3000,
+        'FILTER_ORDER': 5,
+        'SR_COEFFICIENT': 2,
+    },
+    {
+        'N_FFT': 2048,
+        'HOP_LENGTH': 4096,
+        'NUM_PEAKS': 8,
+        'PEAK_SELECTION_STRATEGY': PeakSelection.LOWEST,
+        'N_PEAKS': 4,
         'APPLY_HANNING': True,
         'HANNING_DEGREE': 0.6,
         'INCLUDE_PHASE': True,
@@ -52,8 +72,8 @@ SINE_FIT_CONFIGS = [
         'APPLY_GLIDING': False,
         'GLIDE_THRESHOLD_MAX': 12.0,
         'GLIDE_THRESHOLD_MIN': 5.0,
-        'CUTOFF_FREQUENCY': 3000,
+        'CUTOFF_FREQUENCY': 2000,
         'FILTER_ORDER': 5,
-        'SR_COEFFICIENT': 2,
+        'SR_COEFFICIENT': 1,
     },
 ]
